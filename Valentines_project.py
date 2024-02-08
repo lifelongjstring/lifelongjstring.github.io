df1f21d1fb5185166461b229b0a7fb2bbbd02925
lifelongjstring <159391774+lifelongjstring@users.noreply.github.com>
2024-02-07 18:53:21 -0600

    Revert, did not clear error

--- Valentines_project.py
+++ Valentines_project.py
@@ -1,4 +1,4 @@
-"""from flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,4 +7,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True"""
+    app.run(debug=True
+            
